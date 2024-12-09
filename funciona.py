faeca0ea478fb01859f9e68dfd7e1db9c8769487
BrunoAguia <brunoaguiar14092003@gmail.com>
2024-12-09 11:51:53 -0300

    Add files via upload

---
+++ funciona.py
@@ -0,0 +1,14 @@
+import pyautogui
+
+from time import sleep
+
+pyautogui.click(x=291,y=1076,duration=2)
+pyautogui.click(x=723,y=682,duration=1.5)
+pyautogui.rightClick(x=956,y=769,duration=1)
+pyautogui.click(x=1028,y=679,duration=0.5)
+sleep(0.5)
+pyautogui.click(x=1269,y=692,duration=0.5)
+
+
+
+
